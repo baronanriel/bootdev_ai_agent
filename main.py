756c3cfee0f7405227769c6695c62e5eb4406677
baronanriel <baronanriel@gmail.com>
2025-07-15 07:19:15 -0700

    Adding calculator and some initial tests for scope and capability

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import os
-from sys import argv
-from dotenv import load_dotenv
+import sys
 from google import genai
+from dotenv import load_dotenv
+from google.genai import types
+
 
 def main():
-    print("Hello from bootdev-ai-agent!")
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    try:
+        user_prompt = sys.argv[1]
+    except:
+        print("Fix your prompt")
+        sys.exit(1)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
         model = 'gemini-2.0-flash-001', \
-        contents = f'{argv[1]}' \
+        contents = messages \
     )
 
-    print(f'{response.text}' \
-        f'\nPrompt tokens: {response.usage_metadata.prompt_token_count}' \
-        f'\nResponse tokens: {response.usage_metadata.candidates_token_count}' \
-        )
+    print(f'{response.text}')
+
+    if "--verbose" in sys.argv:
+        print(f'\nUser prompt: {user_prompt}' \
+            f'\nPrompt tokens: {response.usage_metadata.prompt_token_count}' \
+            f'\nResponse tokens: {response.usage_metadata.candidates_token_count}' \
+            )
 
 
 if __name__ == "__main__":
